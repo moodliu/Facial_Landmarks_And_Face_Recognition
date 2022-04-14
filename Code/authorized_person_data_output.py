3da8ad7c7ee52bc337d9f54b08a16cc39969682a
moodliu <a10203082@gmail.com>
2022-04-14 21:44:07 +0800

    Update authorized_person_data_output.py

--- Code/authorized_person_data_output.py
+++ Code/authorized_person_data_output.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
+#import joblib
 from sklearn.externals import joblib
+'''
 pos_image_dir='authorized_person/'
 authorized_person_list=os.listdir(pos_image_dir)
 for person in authorized_person_list:       
@@ -9,4 +11,10 @@ for person in authorized_person_list:
         with open(person + "_face_descriptor_info.txt", "a") as face_descriptor_info:
             print(temp_data[i], file=face_descriptor_info)
             face_descriptor_info.close()
-    
+
+'''
+temp_data=joblib.load('D:/Facial_Landmarks_And_Face_Recognition/authorized_person/10427109/face_descriptor.pkl')
+for i in range(len(temp_data)) :
+    with open("10427109_face_descriptor_info.txt", "a") as face_descriptor_info:
+        print(temp_data[i], file=face_descriptor_info)
+        face_descriptor_info.close()
